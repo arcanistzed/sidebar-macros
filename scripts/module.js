87ee50eb112b8f7a4498d9b7e44ad6af48fcd213
arcanist <arcanistzed@gmail.com>
2021-09-02 16:50:10 -0400

    fix: issue with timing (use hooks)

--- scripts/module.js
+++ scripts/module.js
@@ -1,22 +1,25 @@
-// Move Macros directory to sidebar
-const macros = document.querySelector("#macros");
-macros.style.display = ""; // So that it is displayed properly and not all of the time
-document.querySelector("#sidebar").append(macros);
+Hooks.on("renderSidebarTab", (doc, html) => {
+    if (doc.tabName === "macros") {
+        // Move Macros directory to sidebar
+        html[0].style.display = ""; // So that it is displayed properly and not all of the time
+        document.querySelector("#sidebar").append(html[0]);
 
-// Create Macros tab
-const tab = document.createElement("a");
-tab.classList.add("item");
-tab.title = "Macros";
-tab.dataset.tab = "macros";
+        // Create Macros tab
+        const tab = document.createElement("a");
+        tab.classList.add("item");
+        tab.title = "Macros";
+        tab.dataset.tab = "macros";
 
-// Add icon for tab
-const icon = document.createElement("i");
-icon.classList.add("fas")
-icon.classList.add("fa-code");
-tab.append(icon);
+        // Add icon for tab
+        const icon = document.createElement("i");
+        icon.classList.add("fas")
+        icon.classList.add("fa-code");
+        tab.append(icon);
 
-// Add Macro tab to sidebar before compendiums
-document.querySelector("#sidebar-tabs > [data-tab='compendium']").before(tab);
+        // Add Macro tab to sidebar before compendiums
+        document.querySelector("#sidebar-tabs > [data-tab='compendium']").before(tab);
 
-// Resize sidebar to leave room for the additional tab
-document.querySelector("#sidebar").style.width = "330px";
+        // Resize sidebar to leave room for the additional tab
+        document.querySelector("#sidebar").style.width = "330px";
+    };
+});
