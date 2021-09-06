3e822805283aa31622b6e2086463e48629f14e56
arcanist <arcanistzed@gmail.com>
2021-09-06 17:48:30 -0400

    fix: sidebar not appearing
    Fixes #1

--- scripts/module.js
+++ scripts/module.js
@@ -38,6 +38,7 @@ const onCollapse = collapsed => {
  * @param {HTMLElement} html - The html element of the Macro sidebar tab
  */
 const createDirectory = html => {
+
     // Move Macros directory to sidebar if there isn't already one there
     if (document.querySelectorAll("#macros").length <= 1) document.querySelector("#sidebar").append(html);
 
@@ -59,3 +60,32 @@ const createDirectory = html => {
     // Add Macro tab to sidebar before compendiums if it's not already there
     if (!document.querySelector("#sidebar-tabs > [data-tab='macros']")) document.querySelector("#sidebar-tabs > [data-tab='compendium']").before(tab);
 };
+
+// Override default macro class as the UI for macros
+Hooks.on("init", () => CONFIG.ui.macros = MacroSidebarDirectory);
+
+
+// The following code was largely taken from `foundry.js` to ensure that this module works and is licensed under the Foundry Virtual Tabletop Limited License Agreement for module development
+/**
+ * The directory, displayed in the Sidebar, which organizes and displays world-level Macro documents.
+ * @extends {SidebarDirectory}
+ *
+ * @see {@link MacroDirectory}  The Macro Directory not displayed in the Sidebar
+ * @see {@link Macros}          The WorldCollection of Macro Entities
+ * @see {@link Macro}           The Macro Entity
+ * @see {@link MacroConfig}     The Macro Configuration Sheet
+ */
+class MacroSidebarDirectory extends SidebarDirectory {
+    /** @override */
+    static documentName = "Macro";
+
+    /* -------------------------------------------- */
+
+    /** @override */
+    async _onCreate(event) {
+        event.preventDefault();
+        const name = game.i18n.format("ENTITY.New", { entity: game.i18n.localize("ENTITY.Macro") });
+        const macro = await Macro.create({ name, type: "chat", scope: "global" }, { temporary: true });
+        macro.sheet.render(true);
+    }
+}
